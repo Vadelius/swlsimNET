a73d237a86be4895bd9e5ac4ad1ca9db49edc352
Meaniie <niklasgss@gmail.com>
2017-09-28 19:29:53 +0200

    Fixed forever

--- swlsimNET/Controllers/HomeController.cs
+++ swlsimNET/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ namespace swlsimNET.Controllers
 {
     public class HomeController : Controller
     {
+        private Settings _settings = new Settings();
+
         public IActionResult Index()
         {
             return View();
@@ -18,14 +20,14 @@ namespace swlsimNET.Controllers
 
         public IActionResult Import()
         {
-            return View();
+            return View(_settings);
         }
  
         // POST: Import
         [HttpPost]
-        public ActionResult Import(ServerApp.Models.Player player)
+        public ActionResult Import(Settings setting)
         {
-             try
+            try
             {
                 return View("Import");
             }
@@ -34,18 +36,21 @@ namespace swlsimNET.Controllers
                 return View();
             }
         }
+
         public IActionResult Export()
         {
             ViewData["Message"] = "Export";
 
             return View();
         }
+
         public IActionResult Results()
         {
             ViewData["Message"] = "Results";
 
             return View();
         }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
