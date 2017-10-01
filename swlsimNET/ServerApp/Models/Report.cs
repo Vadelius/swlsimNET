5aa3a2027732893c0c2a0ef41e6242b0080a9e21
victorvadelius <victorvadelius@gmail.com>
2017-10-01 09:45:09 +0200

    Eureka

--- swlsimNET/ServerApp/Models/Report.cs
+++ swlsimNET/ServerApp/Models/Report.cs
@@ -4,9 +4,10 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
+using swlsimNET.Models;
 using swlsimNET.ServerApp.Combat;
+using swlsimNET.ServerApp.Models;
 using swlsimNET.ServerApp.Spells;
 
 namespace swlsimNET.ServerApp.Models
@@ -16,6 +17,7 @@ namespace swlsimNET.ServerApp.Models
         private List<Attack> _allSpellCast = new List<Attack>();
         private List<ISpell> _distinctSpellCast = new List<ISpell>();
         private StringBuilder _oneBuilder = new StringBuilder();
+
         private StringBuilder _twoBuilder = new StringBuilder();
         private NumberFormatInfo nfi;
 
@@ -26,7 +28,7 @@ namespace swlsimNET.ServerApp.Models
         private double lowestDps = double.MaxValue;
         private double highestDps;
 
-        public Tuple<string, string> GenerateReportData(List<FightResult> iterationFightResults)
+        public Tuple<string, string> GenerateReportData(List<FightResult> iterationFightResults, Settings settings)
         {
             InitReportData(iterationFightResults);
 
@@ -51,27 +53,27 @@ namespace swlsimNET.ServerApp.Models
                 serializer.Serialize(writer, iterationFightResults);
             }
 
-            //var critPercent = decimal.Divide(TotalCrits, TotalHits) * 100;
-            //var dps = TotalDamage / Settings.Default.FightLength / Settings.Default.Iterations;
+            var critPercent = decimal.Divide(TotalCrits, TotalHits) * 100;
+            var dps = TotalDamage / settings.FightLength / settings.Iterations;
 
-            //var avgDamage = TotalDamage / Settings.Default.Iterations;
+            var avgDamage = TotalDamage / settings.Iterations;
 
-            GenerateSpellReportData();
+            GenerateSpellReportData(settings);
             //JsonExport(spellType: SpellType.Procc);
             return new Tuple<string, string>(_oneBuilder.ToString(), _twoBuilder.ToString());
         }
 
-        private void GenerateSpellReportData()
+        private void GenerateSpellReportData(Settings settings)
         {
             // Set order here for spell reports
-            SpellTypeReport(SpellType.Cast);
-            SpellTypeReport(SpellType.Channel);
-            SpellTypeReport(SpellType.Dot);
-            SpellTypeReport(SpellType.Instant);
-            SpellTypeReport(SpellType.Buff);
-            SpellTypeReport(SpellType.Procc);
-            SpellTypeReport(SpellType.Gimmick);
-            SpellTypeReport(SpellType.Passive);
+            SpellTypeReport(SpellType.Cast, settings);
+            SpellTypeReport(SpellType.Channel, settings);
+            SpellTypeReport(SpellType.Dot, settings);
+            SpellTypeReport(SpellType.Instant, settings);
+            SpellTypeReport(SpellType.Buff, settings);
+            SpellTypeReport(SpellType.Procc, settings);
+            SpellTypeReport(SpellType.Gimmick, settings);
+            SpellTypeReport(SpellType.Passive, settings);
         }
 
         private void InitReportData(List<FightResult> iterationFightResults)
@@ -103,35 +105,35 @@ namespace swlsimNET.ServerApp.Models
 
                 foreach (var rr in iteration.RoundResults)
                 {
-                    //foreach (var a in rr.Attacks)
-                    //{
-                    //    if (a.IsHit && a.IsCrit)
-                    //    {
-                    //        _oneBuilder.AppendLine($"[{rr.TimeMs.ToString("#,##0,.0s", nfi)}] " +
-                    //                               $"{a.Spell.Name} *{a.Damage.ToString("#,##0,.0K", nfi)}* " +
-                    //                               $"E({rr.PrimaryEnergyEnd}/{rr.SecondaryEnergyEnd}) " +
-                    //                               $"R({rr.PrimaryGimmickEnd}/{rr.SecondaryGimmickEnd})");
-                    //    }
-                    //    else if (a.IsHit && a.Spell.SpellType != SpellType.Procc)
-                    //    {
-                    //        _oneBuilder.AppendLine($"[{rr.TimeMs.ToString("#,##0,.0s", nfi)}] " +
-                    //                               $"{a.Spell.Name} {a.Damage.ToString("#,##0,.0K", nfi)} " +
-                    //                               $"E({rr.PrimaryEnergyEnd}/{rr.SecondaryEnergyEnd}) " +
-                    //                               $"R({rr.PrimaryGimmickEnd}/{rr.SecondaryGimmickEnd})");
-                    //    }
-                    //    else if (a.IsHit && a.Spell.SpellType == SpellType.Procc)
-                    //    {
-                    //        _oneBuilder.AppendLine($"[{rr.TimeMs.ToString("#,##0,.0s", nfi)}] " +
-                    //                               $"[{a.Spell.Name}] proc!");
-                    //    }
+                    foreach (var a in rr.Attacks)
+                    {
+                        //if (a.IsHit && a.IsCrit)
+                        //{
+                        //    _oneBuilder.AppendLine($"[{rr.TimeMs.ToString("#,##0,.0s", nfi)}] " +
+                        //                           $"{a.Spell.Name} *{a.Damage.ToString("#,##0,.0K", nfi)}* " +
+                        //                           $"E({rr.PrimaryEnergyEnd}/{rr.SecondaryEnergyEnd}) " +
+                        //                           $"R({rr.PrimaryGimmickEnd}/{rr.SecondaryGimmickEnd})");
+                        //}
+                        //else if (a.IsHit && a.Spell.SpellType != SpellType.Procc)
+                        //{
+                        //    _oneBuilder.AppendLine($"[{rr.TimeMs.ToString("#,##0,.0s", nfi)}] " +
+                        //                           $"{a.Spell.Name} {a.Damage.ToString("#,##0,.0K", nfi)} " +
+                        //                           $"E({rr.PrimaryEnergyEnd}/{rr.SecondaryEnergyEnd}) " +
+                        //                           $"R({rr.PrimaryGimmickEnd}/{rr.SecondaryGimmickEnd})");
+                        //}
+                        //else if (a.IsHit && a.Spell.SpellType == SpellType.Procc)
+                        //{
+                        //    _oneBuilder.AppendLine($"[{rr.TimeMs.ToString("#,##0,.0s", nfi)}] " +
+                        //                           $"[{a.Spell.Name}] proc!");
+                        //}
 
-                    //    _allSpellCast.Add(a);
+                        _allSpellCast.Add(a);
 
-                    //    if (_distinctSpellCast.All(s => s.Name != a.Spell.Name))
-                    //    {
-                    //        _distinctSpellCast.Add(a.Spell);
-                    //    }
-                    //}
+                        if (_distinctSpellCast.All(s => s.Name != a.Spell.Name))
+                        {
+                            _distinctSpellCast.Add(a.Spell);
+                        }
+                    }
                 }
             }
 
@@ -141,15 +143,13 @@ namespace swlsimNET.ServerApp.Models
             //                       "\r\n#3 Primary/Secondary Energy/Resource(Weapon)");
         }
 
-        private void SpellTypeReport(SpellType spellType, string nameOverride = "")
+        private static string[] _spellnames = new[] {""};
+
+        public IList<TablePopulator> SpellTypeReport(SpellType spellType, Settings settings, List<TablePopulator> list,
+            string nameOverride = "")
         {
             var dSpells = _distinctSpellCast.Where(s => s.SpellType == spellType).ToList();
-
-            if (!dSpells.Any()) return;
-
-            _twoBuilder.AppendLine(string.IsNullOrEmpty(nameOverride)
-                ? $"\r\n----- {spellType} summary normalized per fight -----"
-                : $"\r\n----- {nameOverride} summary normalized per fight -----");
+            list = new List<TablePopulator>();
 
             foreach (var dSpell in dSpells)
             {
@@ -158,65 +158,24 @@ namespace swlsimNET.ServerApp.Models
                 var avgDmg = allOfSameSpellDatas.Average(s => s.Damage);
                 var crits = allOfSameSpellDatas.Count(s => s.IsCrit);
                 var hits = allOfSameSpellDatas.Count(s => s.IsHit);
-
                 // Can't divide int with int, 4.9 will result in 4 etc, either print with decimal or do a correct rounding
-                //var avghits = hits / (double)Settings.Default.Iterations;
-                //var avgcrits = crits / (double)Settings.Default.Iterations;
-                //var cc = decimal.Divide(crits, hits) * 100;
-                //var hdmg = allOfSameSpellDatas.Max(s => s.Damage);
-                //var ldmg = allOfSameSpellDatas.Where(s => s.IsHit).Min(s => s.Damage);
-
+                var avghits = hits / (double) settings.Iterations;
+                var avgcrits = crits / (double) settings.Iterations;
+                var cc = decimal.Divide(crits, hits) * 100;
+                var hdmg = allOfSameSpellDatas.Max(s => s.Damage);
+                var ldmg = allOfSameSpellDatas.Where(s => s.IsHit).Min(s => s.Damage);
                 var ofTotal = alldmg / TotalDamage * 100;
+                var dmgPerSecond = alldmg / settings.FightLength / settings.Iterations;
+                var avgdmgAvarage = avgDmg / settings.FightLength / settings.Iterations;
+                var executes = hits + crits;
+                // [spellName, DPS, DPS%, Executes, DPE, SpellType, Count, Avarage, Crit%]
 
-                //if (dSpell.BaseDamage == 0)
-                //{
-                //    _twoBuilder.AppendLine($"{ofTotal:0.0}% | {dSpell.Name}, hits: {avghits.ToString("#,0.0", nfi)}");
-                //}
-                //else
-                //{
-                //    var output = _twoBuilder.AppendLine(
-                //        $"{ofTotal:0.0}% | {dSpell.Name}, dmg: {avgDmg.ToString("#,##0,.0K", nfi)}, " +
-                //        $"high: {hdmg.ToString("#,##0,.0K", nfi)}, low: {ldmg.ToString("#,##0,.0K", nfi)}," +
-                //        $" hits: {avghits.ToString("#,0.0", nfi)}, crits: {avgcrits.ToString("#,0.0", nfi)} ({cc:0.0}%)");
+                list.Add(new TablePopulator() { Name = dSpell.Name, DamagePerSecond = (int)dmgPerSecond, DpsPercentage = ofTotal, Executes = executes, DamagePerExecution = (int)avgdmgAvarage, SpellType = dSpell.SpellType.ToString(), Count = executes, Avarage = (int)avgdmgAvarage, CritChance = cc});
+    
 
-                //using (var file = File.AppendText(@"c:\json\item.json"))
-                //{
-                //    var serializer = new JsonSerializer();
-                //    serializer.Serialize(file, output);
-                //}
             }
+            return list;
         }
     }
-
-    //private void JsonExport(SpellType spellType)
-    //{
-    //    var dSpells = _distinctSpellCast.Where(s => s.SpellType == spellType).ToList();
-
-    //    foreach (var dSpell in dSpells)
-    //    {
-    //        var allOfSameSpellDatas = _allSpellCast.Where(s => s.Spell.Name == dSpell.Name).ToList();
-    //        var alldmg = allOfSameSpellDatas.Sum(s => s.Damage);
-    //        var avgDmg = allOfSameSpellDatas.Average(s => s.Damage);
-    //        var crits = allOfSameSpellDatas.Count(s => s.IsCrit);
-    //        var hits = allOfSameSpellDatas.Count(s => s.IsHit);
-    //        var avghits = hits / (double)Settings.Default.Iterations;
-    //        var avgcrits = crits / (double)Settings.Default.Iterations;
-    //        var cc = decimal.Divide(crits, hits) * 100;
-    //        var hdmg = allOfSameSpellDatas.Max(s => s.Damage);
-    //        var ldmg = allOfSameSpellDatas.Where(s => s.IsHit).Min(s => s.Damage);
-    //        var ofTotal = alldmg / TotalDamage * 100;
-
-    //        if (dSpell.BaseDamage == 0)
-    //        {
-    //            // File.WriteAllText(@"c:\json\proc.json", String.Empty); (CLEAR OUTPUT SINCE IM APPENDING)
-    //            string procJson = $"[{dSpell.Name} {avghits} {Environment.NewLine}]";
-    //            using (var file = File.AppendText(@"c:\json\proc.json"))
-    //            {
-    //                var serializer = new JsonSerializer();
-    //                serializer.Serialize(file, procJson);
-    //            }
-    //        }
-    //    }
-    //}
 }
 
---
+++ swlsimNET/ServerApp/Models/TablePopulator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using swlsimNET.ServerApp.Models;
+
+namespace swlsimNET.ServerApp.Models
+{
+    public class TablePopulator 
+    {
+        // [spellName, DPS, DPS%, Executes, DPE, SpellType, Count, Avarage, Crit%]
+
+        public string Name { get; set; }
+        public int DamagePerSecond { get; set; }
+        public double DpsPercentage { get; set; }
+        public int Executes { get; set; }
+        public int DamagePerExecution { get; set; }
+        public string SpellType { get; set; }
+        public int Count { get; set; }
+        public int Avarage { get; set; }
+        public decimal CritChance { get; set; }
+
+    }
+}
