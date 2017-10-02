f837bfaa29e657307ec425c9395cdb93c5be0db1
victorvadelius <victorvadelius@gmail.com>
2017-10-02 13:17:48 +0200

    Added ReportContext, Controller and Database Context mapping report items.

---
+++ swlsimNET/Models/ReportContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace swlsimNET.Models
+{
+public class ReportContext : DbContext
+{
+    public ReportContext(DbContextOptions<ReportContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Report> ChartStuff { get; set; }
+
+}
+}
--- swlsimNET/Startup.cs
+++ swlsimNET/Startup.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using swlsimNET.Models;
 
 namespace swlsimNET
 {
@@ -21,6 +19,7 @@ namespace swlsimNET
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ReportContext>(opt => opt.UseInMemoryDatabase("Report"));
             services.AddMvc();
         }
 
