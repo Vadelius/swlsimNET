b726c76f6b4363b21a8d54e1065fe7f53f7de620
victorvadelius <victorvadelius@gmail.com>
2017-10-06 05:13:41 +0200

    Added all relevant AR weapons

--- swlsimNET/ServerApp/Weapons/AssaultRifle.cs
+++ swlsimNET/ServerApp/Weapons/AssaultRifle.cs
@@ -7,8 +7,54 @@ using swlsimNET.ServerApp.Spells;
 
 namespace swlsimNET.ServerApp.Weapons
 {
+
     public class AssaultRifle : Weapon
     {
+        private bool _infernalLoader = false;
+        private bool _ksr43 = false;
+        private double _startedCooking;
+        private double _fusetimer;
+
+        public override void PreAttack(IPlayer player, RoundResult roundResult)
+        {
+            _fusetimer = _fusetimer + player.CurrentTimeSec;
+            if (_ksr43 && _fusetimer >= 3)
+            {
+                GimmickResource = 0;
+            }
+            if (!_ksr43 && _fusetimer >= 5)
+            {
+                GimmickResource = 0;
+            }
+        }
+
+        private void GrenadeCook()
+        {
+            if (_ksr43)
+            {
+                GimmickResource++;
+                _startedCooking = 0;
+                _fusetimer = 0;
+            }
+            if (!_ksr43 && _startedCooking > 5)
+            {
+                GimmickResource++;
+                _startedCooking = 0;
+                _fusetimer = 0;
+            }
+        }
+
+        public override double GetBonusBaseDamage(IPlayer player, ISpell spell, double gimmickBeforeCast)
+        {
+            double bonusBaseDamage = 0;
+            // Infernal Loader Weapon
+            if (_infernalLoader)
+            {
+                bonusBaseDamage += 0.075; // TODO: 7.5% AR damage
+            }
+
+            return bonusBaseDamage;
+        }
         private readonly List<string> _grenadeGenerators = new List<string>
         {
             "FullAuto", "UnveilEssence", "BurstFire"
@@ -24,7 +70,11 @@ namespace swlsimNET.ServerApp.Weapons
             var spellName = spell.Name;
             if (spellName != null && !_grenadeGenerators.Contains(spellName, StringComparer.CurrentCultureIgnoreCase)) return;
 
-            if (Rnd.Next(1, 101) <= 65) GimmickResource++;
+            if (Rnd.Next(1, 101) <= 65)
+            {
+                _startedCooking = player.CurrentTimeSec;
+                GrenadeCook();
+            }
         } 
     }
 }
