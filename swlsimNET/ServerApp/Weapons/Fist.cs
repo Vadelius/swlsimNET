d628dd4ee63e41cd6e9bcb9d1e75b7048379b910
victorvadelius <victorvadelius@gmail.com>
2017-10-06 05:34:10 +0200

    Added all relevant fist weapons.

--- swlsimNET/ServerApp/Weapons/Fist.cs
+++ swlsimNET/ServerApp/Weapons/Fist.cs
@@ -6,7 +6,29 @@ namespace swlsimNET.ServerApp.Weapons
 {
     public class Fist : Weapon
     {
+        private bool _bladedGauntlets = false;
+        private int _bladedStartBonus = 1;
+        private bool _bloodDrinkers = false;
+        private bool _treshingClaws = false;
+
         public bool AllowFrenziedWrathAbilities { get; private set; }
+
+        public override void PreAttack(IPlayer player, RoundResult rr)
+        {
+            if (_bladedGauntlets && _bladedStartBonus == 1)
+            {
+                GimmickResource = +15;
+                _bladedStartBonus = 2;
+            }
+            if (_bladedGauntlets)
+            {
+                GimmickResource = +2;
+            }
+            if (_treshingClaws && AllowFrenziedWrathAbilities)
+            {
+                player.AddBonusAttack(rr, new TreshingClaws(player));
+            }
+        }
 
         public Fist(WeaponType wtype, WeaponAffix waffix) : base(wtype, waffix)
         {
@@ -19,6 +41,19 @@ namespace swlsimNET.ServerApp.Weapons
             {
                 // TODO: Set this variable 
                 AllowFrenziedWrathAbilities = true;
+            }
+            if (_bloodDrinkers && spell.SpellType == SpellType.Dot)
+            {
+                GimmickResource = +3;
+            }
+        }
+        public class TreshingClaws : Spell
+        {
+            public TreshingClaws(IPlayer player)
+            {
+                WeaponType = WeaponType.Fist;
+                SpellType = SpellType.Gimmick;
+                BaseDamage = 0.69;
             }
         }
     }
