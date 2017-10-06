cdf0ac241d169c6f1351417aedd35dda278ccd8c
victorvadelius <victorvadelius@gmail.com>
2017-10-06 15:11:13 +0200

    Refactored out BuffWrapperfrom Player  and made partial.

--- swlsimNET/ServerApp/Models/Player.cs
+++ swlsimNET/ServerApp/Models/Player.cs
@@ -9,7 +9,7 @@ using swlsimNET.ServerApp.Weapons;
 
 namespace swlsimNET.ServerApp.Models
 {
-    public class Player : IPlayer, ICombat
+    public partial class Player : IPlayer, ICombat
     {
         private bool _passivesInitiated;
         public ExpressionContext Context;
@@ -540,34 +540,5 @@ namespace swlsimNET.ServerApp.Models
         // TODO: Can this be made better somehow and still work with APL?
         // Only done since buffs and spells can have same
         // Expressions engine does not like this
-        public class BuffWrapper
-        {
-            private readonly Player _player;
-
-            public BuffWrapper(Player player)
-            {
-                _player = player;
-            }
-
-            public IBuff Exposed => _player.GetBuffFromName("Exposed");
-            public IBuff OpeningShot => _player.GetBuffFromName("OpeningShot");
-            public IBuff Savagery => _player.GetBuffFromName("Savagery");
-            public IBuff UnstoppableForce => _player.GetBuffFromName("UnstoppableForce");
-
-            // Hammer APL specifics
-            public bool Enraged => _player.Rage >= 50;
-            public bool FastAndFurious => _player.GetWeaponFromType(WeaponType.Hammer) is Hammer
-                                              hammer && hammer.FastAndFuriousBonus;
-
-            // Pistol APL specifics
-            public bool RedChambers => _player.GetWeaponFromType(WeaponType.Pistol) is Pistol pistol && 
-                                       pistol.LeftChamber == Chamber.Red && pistol.LeftChamber == pistol.RightChamber;
-            public bool BlueChambers => _player.GetWeaponFromType(WeaponType.Pistol) is Pistol pistol &&
-                                        pistol.LeftChamber == Chamber.Blue && pistol.LeftChamber == pistol.RightChamber;
-            public bool WhiteChambers => _player.GetWeaponFromType(WeaponType.Pistol) is Pistol pistol &&
-                                         pistol.LeftChamber == Chamber.White && pistol.LeftChamber == pistol.RightChamber;
-
-            // TODO: Need to add all buffs here if we cant solve it in another way..
-        }
     }
 }
