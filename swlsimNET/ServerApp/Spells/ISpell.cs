9bfc43d012fe65ce979bd0cc0dec5633cba946be
Meaniie <niklasgss@gmail.com>
2017-10-07 02:40:23 +0200

    ISpell moved out of spell

---
+++ swlsimNET/ServerApp/Spells/ISpell.cs
@@ -0,0 +1,42 @@
+﻿using swlsimNET.ServerApp.Combat;
+using swlsimNET.ServerApp.Models;
+using swlsimNET.ServerApp.Weapons;
+
+namespace swlsimNET.ServerApp.Spells
+{
+    public interface ISpell
+    {
+        string Name { get; }
+        double BaseDamage { get; set; }
+        double DotDuration { get; set; }
+        double DotExpirationBaseDamage { get; set; }
+        double BaseDamageCrit { get; set; }
+        WeaponType WeaponType { get; }
+        SpellType SpellType { get; }
+        AbilityType AbilityType { get; }
+        int PrimaryCost { get; set; }
+        int SecondaryCost { get; set; }
+        int PrimaryGain { get; set; }
+        int SecondaryGain { get; set; }
+        int PrimaryGimmickCost { get; set; }
+        int PrimaryGimmickReduce { get; set; }
+        int SecondaryGimmickCost { get; set; }
+        int SecondaryGimmickReduce { get; set; }
+        int PrimaryGimmickGain { get; set; }
+        int SecondaryGimmickGain { get; set; }
+        int PrimaryGimmickRequirement { get; set; }
+        int PrimaryGimmickGainOnCrit { get; set; }
+        double MaxCooldown { get; set; }
+        double Cooldown { get; set; }
+        double CastTime { get; set; }
+        int ChannelTicks { get; set; }
+        int DotTicks { get; set; }
+        double BonusCritChance { get; set; }
+        double BonusCritPower { get; set; }
+        string Args { get; }
+        Passive PassiveBonusSpell { get; set; }
+        AbilityBuff AbilityBuff { get; set; }
+        Attack Execute(Player player);
+        bool CanExecute(Player player);
+    }
+}
--- swlsimNET/ServerApp/Spells/SpellBase.cs
+++ swlsimNET/ServerApp/Spells/SpellBase.cs
@@ -8,47 +8,6 @@ using swlsimNET.ServerApp.Weapons;
 
 namespace swlsimNET.ServerApp.Spells
 {
-    #region ISpell
-
-    public interface ISpell
-    {
-        string Name { get; }
-        double BaseDamage { get; set; }
-        double DotDuration { get; set; }
-        double DotExpirationBaseDamage { get; set; }
-        double BaseDamageCrit { get; set; }
-        WeaponType WeaponType { get; }
-        SpellType SpellType { get; }
-        AbilityType AbilityType { get; }
-        int PrimaryCost { get; set; }
-        int SecondaryCost { get; set; }
-        int PrimaryGain { get; set; }
-        int SecondaryGain { get; set; }
-        int PrimaryGimmickCost { get; set; }
-        int PrimaryGimmickReduce { get; set; }
-        int SecondaryGimmickCost { get; set; }
-        int SecondaryGimmickReduce { get; set; }
-        int PrimaryGimmickGain { get; set; }
-        int SecondaryGimmickGain { get; set; }
-        int PrimaryGimmickRequirement { get; set; }
-        int PrimaryGimmickGainOnCrit { get; set; }
-        double MaxCooldown { get; set; }
-        double Cooldown { get; set; }
-        double CastTime { get; set; }
-        int ChannelTicks { get; set; }
-        int DotTicks { get; set; }
-        double BonusCritChance { get; set; }
-        double BonusCritPower { get; set; }
-        string Args { get; }
-        Passive PassiveBonusSpell { get; set; }
-        AbilityBuff AbilityBuff { get; set; }
-
-        Attack Execute(Player player);
-        bool CanExecute(Player player);
-    }
-
-    #endregion
-
     public enum SpellType
     {
         Cast,
@@ -58,8 +17,7 @@ namespace swlsimNET.ServerApp.Spells
         Procc,
         Gimmick,
         Dot,
-        Buff,
-
+        Buff
     }
 
     public enum AbilityType
