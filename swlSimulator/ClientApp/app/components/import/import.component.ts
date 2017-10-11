42116e5dde087a608386c13d5b12abd6dcef3c2c
victorvadelius <victorvadelius@gmail.com>
2017-10-12 00:12:43 +0200

    Created two-way databindings for all form properties.

--- swlSimulator/ClientApp/app/components/import/import.component.ts
+++ swlSimulator/ClientApp/app/components/import/import.component.ts
@@ -5,5 +5,23 @@
     templateUrl: './import.component.html'
 })
 export class ImportComponent {
+    primaryWeapon = "";
+    primaryProc = "";  
+    primaryAffix = "";
+    secondaryWeapon = "";
+    secondaryyProc = "";
+    secondaryAffix = "";
+    head = "";
+    neck = "";
+    luck = "";
+    gadget = "";
+    basicSignet = 101;
+    powerSignet = 18;
+    eliteSignet = 31;
+    luckSignet = 12;
+    waistSignet = 52;
+    combatPower = 1300;
+    criticalChance = 31;
+    criticalPower = 110;
 }
 
