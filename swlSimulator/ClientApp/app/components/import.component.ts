ad519c36e59325011f1ec4b647ea9cdf3ab9ff2e
victorvadelius <victorvadelius@gmail.com>
2017-10-14 15:43:22 +0200

    Added all forms.

--- swlSimulator/ClientApp/app/components/import.component.ts
+++ swlSimulator/ClientApp/app/components/import.component.ts
@@ -11,26 +11,17 @@ export class ImportComponent {
     primaryAffix: string[] = ["Havoc", "Destruction", "Effieciency", "Energy", "None"];
     secondaryWeapon: string[] = ["Rifle", "Blade", "Blood", "Chaos", "Elemental", "Fist", "Hammer", "Pistol", "Shotgun"];
     secondaryAffix: string[] = ["Havoc", "Destruction", "Effieciency", "Energy", "None"];
-
-    
+    primaryProc: string []
+    secondaryProc: string[] 
     hammerProc: string[] = ["None", "Anima Touched", "Plasma Forged", "Shadowbound", "Pneumatic Maul", "Fuming Despoiler"]
     fistProc: string[] = ["None", "Anima Touched", "Plasma Forged", "Shadowbound", "Bladed Gauntlets", "Treshing Claws", "Blood Drinkers"]
-    bladeProc: string[] = [""]
-    elementalProc: string[] = [""]
-    bloodProc: string[] = [""]
-    pistolProc: string[] = [""]
-    shotgunProc: string[] = [""]
-    rifleProc: string[] = [""]
-    chaosProc: string[] = [""]
-
-    // None, AnimaTouched, FlameWreathed, PlasmaForged, Shadowbound,
-    // EldritchTome,
-    // MiseryAndMalice, SixShooters, SovTechHarmonisers, Cb3Annihilators, HeavyCaliberPistols,
-    // InfernalLoader, Ksr43,
-    // Apocalypse, RazorsEdge, BladeOfTheSeventhSon, Soulblade,
-    // IfritanDespoiler, Spesc221,
-    // UnstableElectronCore, FrozenFigurine, CryoChargedConduit,
-    // WarpedVisage, OtherworldlyArtifact, SovTechParadoxGenerator
+    bladeProc: string[] = ["None", "Anima Touched", "Plasma Forged", "Shadowbound", "Apocalypse", "Razors Edge", "Blade Of The eventh Son", "Soulblade"]
+    elementalProc: string[] = ["None", "Anima Touched", "Plasma Forged", "Shadowbound", "Unstable Electron Core", "Frozen Figurine", "Cryo Charged Conduit"]
+    bloodProc: string[] = ["None", "Anima Touched", "Plasma Forged", "Shadowbound", "EldritchTome"]
+    pistolProc: string[] = ["None", "Anima Touched", "Plasma Forged", "Shadowbound", "Misery & Malice", "Six Shooters", "Sov-Tech Harmonisers", "CB-3 Annihilators", "Heavy Caliber Pistols"]
+    shotgunProc: string[] = ["None", "Anima Touched", "Plasma Forged", "Shadowbound", "Ifritan Despoiler", "SpesC221"]
+    rifleProc: string[] = ["None", "Anima Touched", "Plasma Forged", "Shadowbound", "Infernal Loader", "KSR43"]
+    chaosProc: string[] = ["None", "Anima Touched", "Plasma Forged", "Shadowbound", "Warped Visage", "Otherwordly Artifact", "Sov Tech Paradox Generator"]
 
     combatPower: 1300;
     criticalChance: 31;
@@ -75,15 +66,18 @@ export class ImportComponent {
             head: new FormControl(),
             neck: new FormControl(),
             luck: new FormControl(),
-            combatPower: new FormControl( [
-                    Validators.required
-            ]),
-            criticalChance: new FormControl([
-                Validators.required
-            ]),
-            criticalPower: new FormControl([
-                Validators.required
-            ]),
+            combatPower: new FormControl(),
+            criticalChance: new FormControl(),
+            criticalPower: new FormControl(),
+            basicSignet: new FormControl(),
+            powerSignet: new FormControl(),
+            eliteSignet: new FormControl(),
+            luckSignet: new FormControl(),
+            wristSignet: new FormControl(),
+            gadget: new FormControl(),
+            apl: new FormControl(),
+            
         });
+        
     }
 }
