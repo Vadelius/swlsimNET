4d4b6b0c155ecdbb08fb14f88fcd38a9a93b07cd
victorvadelius <victorvadelius@gmail.com>
2017-10-17 12:04:37 +0200

    All charts populated

--- swlSimulator/ClientApp/app/components/result.component.ts
+++ swlSimulator/ClientApp/app/components/result.component.ts
@@ -13,73 +13,35 @@ export class ResultComponent implements OnInit {
     HighestDps: string;
     LowestDps: string;
     Dps: string;
-
-    public pieChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
-    public pieChartData:number[] = [300, 500, 100];
+    TotalDps: string;
+    public primary:Array<any> = [];
+    public secondary:Array<any> = [];
+    public pgimmick:Array<any> = [];
+    public sgimmick:Array<any> = [];
+    public breakdownLabels:string[] = [];
+    public breakdownData:number[] = [];
     public pieChartType:string = 'pie';
-      // events
-  public chartClicked(e:any):void {
-    console.log(e);
-    }
- 
-  public chartHovered(e:any):void {
-    console.log(e);
-    }
-    // line chart
 
-      // lineChart
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
-  ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public chartClicked(e:any):void {}
+  public chartHovered(e:any):void {}
+
+public energyChartData:Array<any> = [
+    {data: this.primary, label: 'Primary', borderColor: "#3e95cd"},
+    {data: this.secondary, label: 'Secondary', borderColor: "#c45850"}];
+public gimmickChartData:Array<any> = [
+        {data: this.pgimmick, label: 'Primary', borderColor: "#3e95cd"},
+        {data: this.sgimmick, label: 'Secondary', borderColor: "#c45850"}];
+
+
+  public bgcolors:Array<any> = ["#c45850", "#3e95cd", "#c45850", "#3e95cd", "#c45850", "#90A4AE", "#B0BEC5", "#CFD8DC", "#ECEFF1" ];
+  public lineChartLabels:Array<any> = [];
   public lineChartOptions:any = {
     responsive: true
   };
-  public lineChartColors:Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }
-  ];
+  
   public lineChartLegend:boolean = true;
   public lineChartType:string = 'line';
  
-  public randomize():void {
-    let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-      }
-    }
-    this.lineChartData = _lineChartData;
-  }
- 
-  // events
-
-
     ngOnInit() { 
         
         var results: any = localStorage.getItem("Results");
@@ -88,7 +50,30 @@ export class ResultComponent implements OnInit {
         this.Dps = root.totalDps.toFixed(0);
         this.LowestDps = root.lowestDps.toFixed(0);
         this.HighestDps = root.highestDps.toFixed(0);
+        this.TotalDps = root.totalDamage.toFixed(0);
 
+        this.breakdownData = root.spellBreakdownList
+        .filter(spell => spell.dpsPercentage > 0)
+        .map(spell => spell.dpsPercentage);
+
+        this.breakdownLabels = root.spellBreakdownList
+        .filter(spell => spell.dpsPercentage > 0)
+        .map(spell => spell.name);
+
+        this.lineChartLabels = root.energyList.map(time => time.time);
+
+        for(let pgimmick of root.energyList){
+            this.pgimmick.push(pgimmick.pgimmick);
+        };
+        for(let sgimmick of root.energyList){
+            this.sgimmick.push(sgimmick.sgimmick);
+        };
+        for(let primary of root.energyList){
+            this.primary.push(primary.primary);
+        };
+        for(let secondary of root.energyList){
+            this.secondary.push(secondary.secondary);
+        };
 
 
            
